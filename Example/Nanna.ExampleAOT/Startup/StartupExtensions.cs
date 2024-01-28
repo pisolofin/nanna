8e51ec0cfc9493b4f7fe5ecd92a45fdda5492d18
pisolofin <pisolofin@outlook.com>
2024-01-28 15:34:49 +0100

    Move

---
+++ Example/Nanna.ExampleAOT/Startup/StartupExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Nanna.Example.Application.Persistance;
+using System;
+
+namespace Nanna.ExampleAOT.Startup;
+
+public static class StartupExtensions
+{
+    public static IServiceCollection AddAppDbContext(this IServiceCollection services)
+    {
+        return services.AddDbContext<AppDbContext>(
+            (serviceProvider, options) =>
+            {
+                
+            }
+        );
+    }
+}
