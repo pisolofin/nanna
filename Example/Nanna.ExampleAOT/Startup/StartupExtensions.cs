01358f3788b9da533198b353f7d822cabf6d4959
pisolofin <pisolofin@outlook.com>
2024-01-28 15:43:52 +0100

    PostgreSQL

--- Example/Nanna.ExampleAOT/Startup/StartupExtensions.cs
+++ Example/Nanna.ExampleAOT/Startup/StartupExtensions.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.EntityFrameworkCore;
 using Nanna.Example.Application.Persistance;
-using System;
 
 namespace Nanna.ExampleAOT.Startup;
 
@@ -9,10 +7,12 @@ public static class StartupExtensions
 {
     public static IServiceCollection AddAppDbContext(this IServiceCollection services)
     {
+        const string connectionString = "Host=localhost;Port=5488;Database=example;Username=postgres;Password=postgres_pwd";
+
         return services.AddDbContext<AppDbContext>(
             (serviceProvider, options) =>
             {
-                
+                options.UseNpgsql(connectionString, opt => opt.MigrationsAssembly(typeof(Program).Assembly.FullName));
             }
         );
     }
